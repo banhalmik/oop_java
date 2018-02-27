862f2cf31e06672a248385be0da8bd662855b614
banhalmi <banhalmi@iit421.iit.uni-miskolc.hu>
2018-02-27 16:06:18 +0100

    .

---
+++ masodfoku/src/masodfoku/Masodfoku.java
@@ -0,0 +1,24 @@
+package masodfoku;
+
+public class Masodfoku {
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b=20;
+		int c=2;
+		double discriminant = 0;
+		double x1,x2 = 0;
+		
+		discriminant = Math.sqrt((b * b ) - 4 * a * c);
+		
+		if(Double.isNaN(discriminant)) {
+			System.out.println("Nincs valós megoldás!");
+		} else {
+			x1 = (b * (-1) + discriminant) / 2 * a;
+			x2 = (b * (-1) - discriminant) / 2 * a;
+			System.out.println("A megoldások: x1: " + x1 + " x2: " + x2);
+		}
+		
+		System.out.println(discriminant);
+	}
+}
